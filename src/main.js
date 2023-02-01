3db232031174f7962373c03057468439d484ad73
Goldenprevue <goldenprevue@gmail.com>
2023-02-01 13:40:06 +0900

    [EDIT] 로고 사이즈 수정

--- src/main.js
+++ src/main.js
@@ -1,26 +1,6 @@
 'use strick';
-// const body = document.querySelector('body');
 const logo = document.querySelector('.logo');
-
 const nav = document.querySelector('.navbar');
-
-const profile = document.querySelector('#profile');
-const skills = document.querySelector('#skills');
-const projects = document.querySelector('#projects');
-const comment = document.querySelector('#comment');
-const contact = document.querySelector('#contact');
-
-const profileBtn = document.querySelectorAll('.profile-btn, .m-profile');
-console.log(profileBtn);
-const skillsBtn = document.querySelector('.skills-btn');
-console.log(skillsBtn);
-const projectsBtn = document.querySelector('.projects-btn');
-const commentBtn = document.querySelector('.comment-btn');
-const contactBtn = document.querySelector('.contact-btn');
-
-const profileCircle = document.querySelector('.m-profile');
-
-const navbarHeight = nav.getBoundingClientRect().height;
 
 // 로고를 클릭시 메인화면(첫화면)으로 넘어가는 기능
 logo.addEventListener('click', () => {
@@ -30,11 +10,11 @@ logo.addEventListener('click', () => {
 // 스크롤 이벤트가 발생시 지정된 위치에서 Nav bar가 나오는 함수
 document.addEventListener('scroll', () => {
   if (window.scrollY > 685) {
-    nav.classList.toggle('nav-hidden', false);
-    nav.style.transition = 'all 0.7s ease-in-out 2s';
+    nav.classList.remove('nav-hidden');
+    nav.classList.add('nav-show');
   } else {
     nav.classList.add('nav-hidden');
-    nav.classList.toggle('nav-show', true);
+    nav.classList.remove('nav-show');
   }
 });
 
@@ -47,6 +27,31 @@ document.addEventListener('click', (e) => {
     return;
   } else {
     const moveScroll = document.querySelector(target);
-    moveScroll.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    moveScroll.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 });
+
+//circle 사라지게 하기
+const main = document.querySelector('#main');
+const height = main.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  main.style.opacity = 0.8 - window.scrollY / height;
+  console.log(height);
+});
+
+// const body = document.querySelector('body');
+// const profileCircle = document.querySelector('.m-profile');
+// const navbarHeight = nav.getBoundingClientRect().height;
+
+// const profile = document.querySelector('#profile');
+// const skills = document.querySelector('#skills');
+// const projects = document.querySelector('#projects');
+// const comment = document.querySelector('#comment');
+// const contact = document.querySelector('#contact');
+
+// const profileBtn = document.querySelectorAll('.profile-btn, .m-profile');
+
+// const skillsBtn = document.querySelector('.skills-btn');
+// const projectsBtn = document.querySelector('.projects-btn');
+// const commentBtn = document.querySelector('.comment-btn');
+// const contactBtn = document.querySelector('.contact-btn');
